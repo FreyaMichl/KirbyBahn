5bb37726a2e299d89b1b199b2affc9158ff4f9ea
Freya Michl <hansermichl.dh@gmail.com>
2021-06-01 10:52:12 +0200

    changed kirby jumpdirection

--- sketch.js
+++ sketch.js
@@ -25,20 +25,30 @@ function setup() {
   engine = Engine.create();
 
   engine.world.gravity.scale = 0.0
-  Matter.Resolver._restingThresh = 0.001
-  MatterAttractors.Attractors.gravityConstant = 0.00081
+  Matter.Resolver._restingThresh = 0.002
+  MatterAttractors.Attractors.gravityConstant = 0.003
 
-  console.log(Matter)
   // create a body with an attractor
   player = Bodies.circle(
-    700,
-    100,
+    (windowWidth / 2),
+    120,
     10, {
       mass: 0.01,
       restitution: 0.7,
+      frictionAir: 0,
       // example of an attractor function that
       // returns a force vector that applies to bodyB
       plugin: {
+        wrap: {
+          min: {
+            x: 0,
+            y: 0
+          },
+          max: {
+            x: windowWidth,
+            y: windowHeight
+          }
+        },
         attractors: [
           function(bodyA, bodyB) {
             if (bodyB.isParticle != true) {
@@ -67,14 +77,14 @@ function setup() {
       windowWidth / 2,
       100,
       50, {
-        mass: 1,
+        mass: 2,
         restitution: 0.7,
       }),
     Bodies.circle(
       windowWidth / 2,
       500,
       50, {
-        mass: 1,
+        mass: 2,
         restitution: 0.7,
       })
   ];
@@ -88,31 +98,49 @@ function setup() {
   Engine.run(engine);
   Events.on(engine, "afterTick", function() {
     if (jumping) {
+      let finalForce = Vector.create(0, 0);
+
       engine.world.bodies.forEach((body, i) => {
         if (body === player) return;
         if (Matter.SAT.collides(player, body).collided) {
-          Body.applyForce(player, player.position, Vector.create(0, 0.00008))
+          var vw = windowWidth / 100;
+          var vh = windowHeight / 100;
+          // use Newton's law of gravitation
+          var bToA = Matter.Vector.sub(body.position, player.position),
+            distanceSq = Matter.Vector.magnitudeSquared(bToA) || 0.001;
+          distanceSq /= 600;
+          var normal = Matter.Vector.normalise(bToA),
+            magnitude = -MatterAttractors.Attractors.gravityConstant * (player.mass * body.mass / distanceSq),
+            force = Matter.Vector.mult(normal, magnitude);
+          force.x /= vw / 16;
+          force.y /= vw / 16;
+          Vector.add(finalForce, force, finalForce);
         }
       });
+      finalForce = Vector.normalise(finalForce);
+      finalForce = Vector.div(finalForce, 5000);
+      Body.applyForce(player, player.position, finalForce)
 
     }
   })
 }
 
 function keyPressed() {
+  //32 is space
   if (keyCode == 32) {
     jumping = true;
   }
 }
 
 function keyReleased() {
+  //32 is space
   if (keyCode == 32) {
     jumping = false;
   }
 }
 
 function draw() {
-  background(0);
+  clear();
   fill(255);
 
   engine.world.bodies.forEach((body, i) => {
