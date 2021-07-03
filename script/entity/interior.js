923f3a7ec9ec7d3566b012ad3521fb81a6da9db2
Freya Michl <hansermichl.dh@gmail.com>
2021-07-03 14:52:48 +0200

    added interior to scene

---
+++ script/entity/interior.js
@@ -0,0 +1,41 @@
+import Entity from "../entity.js";
+import Sprite from "../sprite.js";
+
+export default class Interior extends Entity {
+
+  createSprite() {
+    let sprite = new class extends Sprite {
+
+      createBody(animation) {
+        let body = super.createBody(animation);
+        Body.setPosition(body, Vector.create(910, 1722));
+        return body;
+      }
+    }(loadJSON("assets/sprites/spaceship_whole_interior.json"), {
+      isStatic: true,
+      mass: 0.01
+    });
+    sprite.setAnimation("idle")
+    return sprite;
+  }
+
+  preload() {
+    super.preload();
+    this.actualOutline = loadImage("assets/textures/interiorSpaceship/whole_interior.png")
+  }
+
+  draw() {
+    super.draw();
+    if (this.sprite.body) {
+      let width = 432 * 2;
+      let height = 1227 * 2;
+      image(
+        this.actualOutline,
+        this.sprite.body.position.x - width / 2 + 7 * 2,
+        this.sprite.body.position.y - height / 2 + 79 * 2,
+        width,
+        height)
+    }
+  }
+
+}
--- script/scene/space-scene.js
+++ script/scene/space-scene.js
@@ -2,57 +2,60 @@ import Scene from "../scene.js";
 import Kirby from "../entity/kirby.js";
 import Planet from "../entity/planet.js";
 import Spaceship from "../entity/spaceship.js";
+import Interior from "../entity/interior.js";
 
 class SpaceScene extends Scene {
 
-    constructor() {
-        super();
-        this.spaceship = new Spaceship();
-        this.kirby = new Kirby();
-        this.planet1 = new Planet("assets/textures/planet.png", 1500, 200, 250);
-        this.planet2 = new Planet("assets/textures/erdePlanet.png", 400, 400, 200);
-        this.addEntity(this.planet1);
-        this.addEntity(this.planet2);
-        this.addEntity(this.spaceship);
-        this.addEntity(this.kirby);
-    }
+  constructor() {
+    super();
+    this.interior = new Interior();
+    this.spaceship = new Spaceship();
+    this.kirby = new Kirby();
+    this.planet1 = new Planet("assets/textures/interactionElements/planet.png", 1500, 200, 250);
+    this.planet2 = new Planet("assets/textures/interactionElements/earth_planet.png", 400, 400, 200);
+    this.addEntity(this.planet1);
+    this.addEntity(this.planet2);
+    this.addEntity(this.spaceship);
+    this.addEntity(this.interior);
+    this.addEntity(this.kirby);
+  }
 
-    afterTick() {
-        super.afterTick();
-        let sprite = this.kirby.sprite;
-        if (!sprite) return;
-        let body = sprite.body;
-        if (!body) return;
-        let page = Math.floor((body.position.y + 60) / windowHeight);
-        if (keyIsDown(38)) {
-            if (Matter.SAT.collides(this.planet1.sprite.body, body).collided) {
-                Body.applyForce(body, body.position, Vector.mult(Vector.normalise(Vector.sub(this.planet2.sprite.body.position, body.position)), 0.1))
-            }
-            if (Matter.SAT.collides(this.planet2.sprite.body, body).collided) {
-                Body.applyForce(body, body.position, Vector.mult(Vector.normalise(Vector.sub(Vector.add(this.spaceship.sprite.body.position, Vector.create(300, 0)), body.position)), 0.1))
-            }
-        }
-        body.angle = 0;
-        environment.scrollToPage(page);
+  afterTick() {
+    super.afterTick();
+    let sprite = this.kirby.sprite;
+    if (!sprite) return;
+    let body = sprite.body;
+    if (!body) return;
+    let page = Math.floor((body.position.y + 60) / windowHeight);
+    if (keyIsDown(38)) {
+      if (Matter.SAT.collides(this.planet1.sprite.body, body).collided) {
+        Body.applyForce(body, body.position, Vector.mult(Vector.normalise(Vector.sub(this.planet2.sprite.body.position, body.position)), 0.1))
+      }
+      if (Matter.SAT.collides(this.planet2.sprite.body, body).collided) {
+        Body.applyForce(body, body.position, Vector.mult(Vector.normalise(Vector.sub(Vector.add(this.spaceship.sprite.body.position, Vector.create(300, 0)), body.position)), 0.1))
+      }
     }
+    body.angle = 0;
+    environment.scrollToPage(page);
+  }
 
 
-    preload() {
-        this.background = loadImage("assets/textures/background.png",
-            result => {
-                console.log(this.background.width);
-                this.background.resize(this.background.width, this.background.height);
-            });
-        this.entities.forEach(entity => entity.preload())
-    }
+  preload() {
+    this.background = loadImage("assets/textures/background.png",
+      result => {
+        console.log(this.background.width);
+        this.background.resize(this.background.width, this.background.height);
+      });
+    this.entities.forEach(entity => entity.preload())
+  }
 
-    draw() {
-        image(this.background, 0, 0);
-        super.draw()
-        this.entities.forEach(entity => {
-            entity.draw()
-        });
-    }
+  draw() {
+    image(this.background, 0, 0);
+    super.draw()
+    this.entities.forEach(entity => {
+      entity.draw()
+    });
+  }
 
 
 }
