4cb964c3c8c471fae19941a81756db437cf6ff78
Freya Michl <hansermichl.dh@gmail.com>
2021-07-06 17:52:17 +0200

    added portal

---
+++ script/entity/interactionElements/portal.js
@@ -0,0 +1,46 @@
+import Entity from "../../entity.js";
+import {
+  SimpleSprite
+} from "../../sprite.js";
+
+class PortalSprite extends SimpleSprite {
+  constructor(texture, x, y) {
+    super(texture)
+    this.x = x
+    this.y = y
+  }
+  createBody() {
+    let body = Bodies.rectangle(
+      this.x,
+      this.y,
+      350,
+      160, {
+        isStatic: true,
+        mass: 10,
+        restitution: 0.7,
+      });
+    return body;
+  }
+  draw() {
+    super.draw();
+  }
+}
+
+export default class Portal extends Entity {
+
+  constructor(texture, x, y) {
+    super();
+    this.texture = texture;
+    this.x = x;
+    this.y = y;
+  }
+
+  createSprite() {
+    return new PortalSprite(this.texture, this.x, this.y);
+  }
+
+
+  afterTick() {
+
+  }
+}
--- script/scene/space-scene.js
+++ script/scene/space-scene.js
@@ -8,182 +8,185 @@ import Tomato from "../entity/interactionElements/tomato.js";
 import Tree from "../entity/interactionElements/tree.js";
 import Magnet from "../entity/interactionElements/magnet.js";
 import Crystal from "../entity/interactionElements/crystal.js";
+import Portal from "../entity/interactionElements/portal.js";
 
 class SpaceScene extends Scene {
 
-    constructor() {
-        super();
-        this.tomato1 = new Tomato("assets/textures/interactionElements/tomato.png", 530, 2830);
-        this.tomato2 = new Tomato("assets/textures/interactionElements/tomato.png", 750, 2855);
-        this.treelittle = new Tree("assets/textures/interactionElements/tree_little.png", 100, 100, 33, 60)
-        this.treemiddle = new Tree("assets/textures/interactionElements/tree_middle.png", 200, 100, 44, 77)
-        this.treebig = new Tree("assets/textures/interactionElements/tree_big.png", 300, 100, 53, 94)
-        this.interior = new Interior();
-        this.spaceship = new Spaceship();
-        this.kirby = new Kirby();
-        this.planet1 = new Planet("assets/textures/interactionElements/planet.png", 1500, 400, 250);
-        this.planet2 = new Planet("assets/textures/interactionElements/earth_planet.png", 400, 700, 200);
-        this.magnet = new Magnet();
-        this.crystal1Left = new Crystal("one_left", 0);
-        this.crystal1Right = new Crystal("one_right", 60);
-        this.crystal2Left = new Crystal("two_left", 120);
-        this.crystal2Right = new Crystal("two_right", 200);
-        this.addEntity(this.planet1);
-        this.addEntity(this.planet2);
-        this.addEntity(this.spaceship);
-        this.addEntity(this.interior);
-        this.addEntity(this.kirby);
-        this.addEntity(this.tomato1);
-        this.addEntity(this.tomato2);
-        this.addEntity(this.treelittle);
-        this.addEntity(this.treemiddle);
-        this.addEntity(this.treebig);
-        this.addEntity(this.magnet);
-        this.addEntity(this.crystal1Left);
-        this.addEntity(this.crystal1Right);
-        this.addEntity(this.crystal2Left);
-        this.addEntity(this.crystal2Right);
-        this.createBridge();
-    }
+  constructor() {
+    super();
+    this.tomato1 = new Tomato("assets/textures/interactionElements/tomato.png", 530, 2830);
+    this.tomato2 = new Tomato("assets/textures/interactionElements/tomato.png", 750, 2855);
+    this.treelittle = new Tree("assets/textures/interactionElements/tree_little.png", 100, 100, 33, 60)
+    this.treemiddle = new Tree("assets/textures/interactionElements/tree_middle.png", 200, 100, 44, 77)
+    this.treebig = new Tree("assets/textures/interactionElements/tree_big.png", 300, 100, 53, 94)
+    this.interior = new Interior();
+    this.spaceship = new Spaceship();
+    this.kirby = new Kirby();
+    this.planet1 = new Planet("assets/textures/interactionElements/planet.png", 1500, 400, 250);
+    this.planet2 = new Planet("assets/textures/interactionElements/earth_planet.png", 400, 700, 200);
+    this.magnet = new Magnet();
+    this.crystal1Left = new Crystal("one_left", 0);
+    this.crystal1Right = new Crystal("one_right", 60);
+    this.crystal2Left = new Crystal("two_left", 120);
+    this.crystal2Right = new Crystal("two_right", 200);
+    this.portal = new Portal("assets/textures/interactionElements/portal.png", 890, 5200)
+    this.addEntity(this.planet1);
+    this.addEntity(this.planet2);
+    this.addEntity(this.spaceship);
+    this.addEntity(this.interior);
+    this.addEntity(this.kirby);
+    this.addEntity(this.tomato1);
+    this.addEntity(this.tomato2);
+    this.addEntity(this.treelittle);
+    this.addEntity(this.treemiddle);
+    this.addEntity(this.treebig);
+    this.addEntity(this.magnet);
+    this.addEntity(this.crystal1Left);
+    this.addEntity(this.crystal1Right);
+    this.addEntity(this.crystal2Left);
+    this.addEntity(this.crystal2Right);
+    this.addEntity(this.portal);
+    this.createBridge();
+  }
 
-    createBridge() {
-        let parts = []
-        for (let i = 0; i < 20; i++) {
-            parts[i] = new Bridge("assets/textures/interactionElements/bridge_wood.png", 540 + i * 23, 2130, () => {
-                let done = true;
-                for (let j = 0; j < parts.length; j++) {
-                    if (j === i) continue
-                    if (!parts[j].initialized) {
-                        done = false
-                        break
-                    }
-                }
-                parts[i].initialized = true
-                if (done) {
-                    for (let j = 0; j < parts.length - 1; j++) {
-                        let bodyA = parts[j].sprite.body;
-                        let bodyB = parts[j + 1].sprite.body;
-                        Matter.World.add(environment.engine.world, Matter.Constraint.create({
-                            bodyA: bodyA,
-                            pointA: {
-                                x: 10.5,
-                                y: 0
-                            },
-                            bodyB: bodyB,
-                            pointB: {
-                                x: -10.5,
-                                y: 0
-                            },
-                            restitution: 1,
-                            stiffness: 0.4,
-                            damping: 0.0,
-                            length: 2
-                        }))
-                    }
-
-                    Matter.World.add(environment.engine.world, Matter.Constraint.create({
-                        bodyB: parts[0].sprite.body,
-                        pointA: {
-                            x: 320,
-                            y: 2130,
-                            length: 100,
-                            stiffness: 0.4,
-                            restitution: 1,
-                            damping: 0
-                        },
-                        pointB: {
-                            x: -10.5,
-                            y: 0
-                        },
-                        stiffness: 1,
-                        length: 2
-                    }))
-                    Matter.World.add(environment.engine.world, Matter.Constraint.create({
-                        bodyB: parts[parts.length - 1].sprite.body,
-                        pointA: {
-                            x: 800,
-                            y: 2130,
-                            length: 100,
-                            stiffness: 1
-                        },
-                        pointB: {
-                            x: 10.5,
-                            y: 0
-                        },
-                        stiffness: 1,
-                        length: 2
-                    }))
-                }
-            });
+  createBridge() {
+    let parts = []
+    for (let i = 0; i < 20; i++) {
+      parts[i] = new Bridge("assets/textures/interactionElements/bridge_wood.png", 540 + i * 23, 2130, () => {
+        let done = true;
+        for (let j = 0; j < parts.length; j++) {
+          if (j === i) continue
+          if (!parts[j].initialized) {
+            done = false
+            break
+          }
         }
-
-        parts.forEach(bridge => this.addEntity(bridge))
-
-        return {
-            parts: parts
-        }
-    }
-
-    afterTick() {
-        super.afterTick();
-        let sprite = this.kirby.sprite;
-        if (!sprite) return;
-        let body = sprite.body;
-        if (!body) return;
-        let page = Math.floor((body.position.y + 60) / windowHeight);
-        if (keyIsDown(32)) {
-            if (Matter.SAT.collides(this.planet1.sprite.body, body).collided) {
-                Body.applyForce(body, body.position, Vector.mult(Vector.normalise(Vector.sub(this.planet2.sprite.body.position, body.position)), 0.02))
-            }
-            if (Matter.SAT.collides(this.planet2.sprite.body, body).collided) {
-                Body.applyForce(body, body.position, Vector.mult(Vector.normalise(Vector.sub(Vector.add(this.spaceship.sprite.body.position, Vector.create(300, 0)), body.position)), 0.02))
-            }
-        }
-        environment.scrollToPage(page);
-    }
-
-
-    preload() {
-        this.background = loadImage("assets/textures/background.png",
-            result => {
-                this.background.resize(this.background.width, this.background.height);
-            });
-        this.entities.forEach(entity => entity.preload())
-    }
-
-    draw() {
-        image(this.background, 0, 0);
-        super.draw()
-        this.entities.forEach(entity => {
-            entity.draw()
-        });
-        environment.engine.world.constraints.forEach(constraint => {
-            const offsetA = constraint.pointA;
-            let posA = {
-                x: 0,
+        parts[i].initialized = true
+        if (done) {
+          for (let j = 0; j < parts.length - 1; j++) {
+            let bodyA = parts[j].sprite.body;
+            let bodyB = parts[j + 1].sprite.body;
+            Matter.World.add(environment.engine.world, Matter.Constraint.create({
+              bodyA: bodyA,
+              pointA: {
+                x: 10.5,
                 y: 0
-            };
-            if (constraint.bodyA) {
-                posA = constraint.bodyA.position;
-            }
-            const offsetB = constraint.pointB;
-            let posB = {
-                x: 0,
+              },
+              bodyB: bodyB,
+              pointB: {
+                x: -10.5,
                 y: 0
-            };
-            if (constraint.bodyB) {
-                posB = constraint.bodyB.position;
-            }
-            stroke(255);
+              },
+              restitution: 1,
+              stiffness: 0.4,
+              damping: 0.0,
+              length: 2
+            }))
+          }
 
-            line(
-                posA.x + offsetA.x,
-                posA.y + offsetA.y,
-                posB.x + offsetB.x,
-                posB.y + offsetB.y
-            );
-        })
+          Matter.World.add(environment.engine.world, Matter.Constraint.create({
+            bodyB: parts[0].sprite.body,
+            pointA: {
+              x: 320,
+              y: 2130,
+              length: 100,
+              stiffness: 0.4,
+              restitution: 1,
+              damping: 0
+            },
+            pointB: {
+              x: -10.5,
+              y: 0
+            },
+            stiffness: 1,
+            length: 2
+          }))
+          Matter.World.add(environment.engine.world, Matter.Constraint.create({
+            bodyB: parts[parts.length - 1].sprite.body,
+            pointA: {
+              x: 800,
+              y: 2130,
+              length: 100,
+              stiffness: 1
+            },
+            pointB: {
+              x: 10.5,
+              y: 0
+            },
+            stiffness: 1,
+            length: 2
+          }))
+        }
+      });
     }
+
+    parts.forEach(bridge => this.addEntity(bridge))
+
+    return {
+      parts: parts
+    }
+  }
+
+  afterTick() {
+    super.afterTick();
+    let sprite = this.kirby.sprite;
+    if (!sprite) return;
+    let body = sprite.body;
+    if (!body) return;
+    let page = Math.floor((body.position.y + 60) / windowHeight);
+    if (keyIsDown(32)) {
+      if (Matter.SAT.collides(this.planet1.sprite.body, body).collided) {
+        Body.applyForce(body, body.position, Vector.mult(Vector.normalise(Vector.sub(this.planet2.sprite.body.position, body.position)), 0.02))
+      }
+      if (Matter.SAT.collides(this.planet2.sprite.body, body).collided) {
+        Body.applyForce(body, body.position, Vector.mult(Vector.normalise(Vector.sub(Vector.add(this.spaceship.sprite.body.position, Vector.create(300, 0)), body.position)), 0.02))
+      }
+    }
+    environment.scrollToPage(page);
+  }
+
+
+  preload() {
+    this.background = loadImage("assets/textures/background.png",
+      result => {
+        this.background.resize(this.background.width, this.background.height);
+      });
+    this.entities.forEach(entity => entity.preload())
+  }
+
+  draw() {
+    image(this.background, 0, 0);
+    super.draw()
+    this.entities.forEach(entity => {
+      entity.draw()
+    });
+    environment.engine.world.constraints.forEach(constraint => {
+      const offsetA = constraint.pointA;
+      let posA = {
+        x: 0,
+        y: 0
+      };
+      if (constraint.bodyA) {
+        posA = constraint.bodyA.position;
+      }
+      const offsetB = constraint.pointB;
+      let posB = {
+        x: 0,
+        y: 0
+      };
+      if (constraint.bodyB) {
+        posB = constraint.bodyB.position;
+      }
+      stroke(255);
+
+      line(
+        posA.x + offsetA.x,
+        posA.y + offsetA.y,
+        posB.x + offsetB.x,
+        posB.y + offsetB.y
+      );
+    })
+  }
 
 
 }
