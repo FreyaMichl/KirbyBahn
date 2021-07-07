6f292b4420414959e57fd7bea8daa4c7fb890b18
Freya Michl <hansermichl.dh@gmail.com>
2021-07-07 22:03:42 +0200

    added motor blades

--- script/entity/interactionElements/rotor.js
+++ script/entity/interactionElements/rotor.js
@@ -1,29 +1,48 @@
 import Entity from "../../entity.js";
 import {
-    SimpleSprite
+  SimpleSprite
 } from "../../sprite.js";
 
+let angle = 0;
 
-export default class Portal extends Entity {
+export default class Rotor extends Entity {
 
-    constructor(texture, x, y) {
-        super();
-        this.texture = texture;
+  constructor(texture, x, y, direction) {
+    super();
+    this.texture = texture;
+    this.x = x;
+    this.y = y;
+    this.direction = direction;
+    this.angle = 0
+  }
+
+  createSprite() {
+    return new class extends SimpleSprite {
+
+      constructor(texture, x, y, direction) {
+        super(texture);
         this.x = x;
         this.y = y;
+        this.direction = direction;
+      }
+
+      createBody() {
+        let body = Bodies.rectangle(this.x, this.y, 122, 10, {
+          isStatic: true,
+          mass: 0.5,
+          restitution: 0.7
+        });
+        return body
+      }
+    }(this.texture, this.x, this.y, this.direction);
+  }
+
+  afterTick() {
+    if (!this?.sprite?.body) {
+      return
     }
-
-    createSprite() {
-        return new class extends SimpleSprite{
-
-            createBody() {
-                let body = Bodies.rectangle(100, 100, )
-            }
-        }(this.texture, this.x, this.y);
-    }
-
-
-    afterTick() {
-
-    }
+    Body.setAngle(this.sprite.body, this.angle);
+    Body.setAngularVelocity(this.sprite.body, 0.15);
+    this.angle += this.direction;
+  }
 }
